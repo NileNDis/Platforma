cd84eda0c9a69b350e1c90aa670a8f23e7332e5c
NileNDis <denis.gg2018@gmail.net>
2022-04-25 17:50:43 +0300

    Add project files.

---
+++ Intermediate/Build/Win64/PlatformaEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.29.30142
+#include "D:/C++/games/Platforma/Intermediate/Build/Win64/PlatformaEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ Intermediate/Build/Win64/UnrealEditor/Inc/Platforma/Platforma.init.gen.cpp
@@ -0,0 +1,29 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodePlatforma_init() {}
+	static FPackageRegistrationInfo Z_Registration_Info_UPackage__Script_Platforma;
+	FORCENOINLINE UPackage* Z_Construct_UPackage__Script_Platforma()
+	{
+		if (!Z_Registration_Info_UPackage__Script_Platforma.OuterSingleton)
+		{
+			static const UECodeGen_Private::FPackageParams PackageParams = {
+				"/Script/Platforma",
+				nullptr,
+				0,
+				PKG_CompiledIn | 0x00000000,
+				0x71B4D7E0,
+				0xC002EA20,
+				METADATA_PARAMS(nullptr, 0)
+			};
+			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_Platforma.OuterSingleton, PackageParams);
+		}
+		return Z_Registration_Info_UPackage__Script_Platforma.OuterSingleton;
+	}
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_Platforma(Z_Construct_UPackage__Script_Platforma, TEXT("/Script/Platforma"), Z_Registration_Info_UPackage__Script_Platforma, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x71B4D7E0, 0xC002EA20));
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
